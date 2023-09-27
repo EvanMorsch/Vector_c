d4536b16df248baa997ce70e328ea791a85a8a38
Evan Morschhauser <evanmorsch@gmail.com>
2023-09-26 22:35:09 -0400

    Populate header

--- vec2.h
+++ vec2.h
@@ -17,4 +17,11 @@ struct Vector2D_t
 	double y;
 };
 
+Vector2D_t* Vector2D_Create(double, double);
+void Vector2D_Destroy(Vector2D_t*);
+
+void Vector2D_Set(Vector2D_t*, double, double);
+
+Vector2D_t* Vector2D_Copy(Vector2D_t* );
+
 #endif
