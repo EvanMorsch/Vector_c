ae562f5e08d6d99866d01dfe30ec4f9b85590c6e
Evan Morschhauser <evanmorsch@gmail.com>
2023-09-26 22:19:38 -0400

    Init comit

---
+++ vec2.c
@@ -0,0 +1,15 @@
+/* 	@file vec2.c
+ *  @brief
+ */
+ 
+#include "vec2.h"
+
+/*
+ *  @brief The stucture of a 2 dimensional vector.
+ */
+typedef struct Vector2D_t Vector2D_t;
+struct Vector2D_t
+{
+	double x;
+	double y;
+};
---
+++ vec2.h
@@ -0,0 +1,18 @@
+/* 	@file vec2.h
+ *  @brief
+ */
+ 
+#ifndef vec2_h
+#define vec2_h
+ 
+/*
+ *  @brief The stucture of a 2 dimensional vector.
+ */
+typedef struct Vector2D_t Vector2D_t;
+struct Vector2D_t
+{
+	double x;
+	double y;
+};
+
+#endif
